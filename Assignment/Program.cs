cae256d26c8c986b241807b8bd68af9001cc1198
Mics90 <naurismics@gmail.com>
2023-02-06 11:37:27 +0000

    Initial commit

---
+++ Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write(" Welcome to Acme Accounting Systems \n");
+            
+            Console.Write("Remember, we’re \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
