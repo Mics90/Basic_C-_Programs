67c0787cc5f1071b4d2fcdef2bb38d6c7f06ea97
Nauris Mics <109521261+Mics90@users.noreply.github.com>
2023-02-13 14:02:34 +0000

    Boolean assignment

---
+++ Boolean assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is Your age?");
+            int age = Convert.ToInt32(Console.ReadLine()); //converts user input to int
+            
+            Console.WriteLine("Have you ever had a DUI violation? Please answer with True or False");
+            bool violation = Convert.ToBoolean(Console.ReadLine());// converts user input to boolean
+            Console.WriteLine("How many speeding tickets have you had?");
+            int tickets = Convert.ToInt32(Console.ReadLine()); // converts user input to int
+            Console.WriteLine("Qualified? : " + (violation == false && tickets < 3 && age > 15)) ; //checks if qualified with AND operators
+            Console.ReadLine();
+        }
+    }
+}
