5a15dd2016b9c9f380da495def239778008b76d1
Nauris Mics <109521261+Mics90@users.noreply.github.com>
2023-02-20 13:52:05 +0000

    Iteration examples

--- Iteration/Program.cs
+++ Iteration/Program.cs
@@ -20,16 +20,62 @@ namespace Iteration
             //}
             //Console.ReadLine();
 
-            string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
+            //string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
 
-            for (int j = 0; j < names.Length; j++)
+            //for (int j = 0; j < names.Length; j++)
+            //{
+            //    //if (names[j] == "Jesse")
+            //    {
+            //        Console.WriteLine(names[j]);
+
+            //    }
+            //}
+            //Console.Read();
+
+            //List<int> testScores = new List<int>();
+            //testScores.Add(98);
+            //testScores.Add(99);
+            //testScores.Add(81);
+            //testScores.Add(72);
+            //testScores.Add(70);
+
+            //foreach (int score in testScores)
+            //{
+            //    if (score > 85)
+            //    {
+            //        Console.WriteLine("Passing test score: " + score);
+            //    }    
+            //}
+            //Console.ReadLine();
+
+            //List<string> names = new List<string>() { "Jesse", "Erik", "Adam", "Daniel" };
+
+            //foreach (string name in names)
+            //{
+            //    //if ( name == "Jesse")
+            //    {
+            //        Console.WriteLine(name);
+
+            //    }
+            //}
+            //Console.ReadLine();
+
+            List<int> testScores = new List<int>() { 98, 99, 12, 74, 23, 99 };
+            List<int> passingScores = new List<int>();
+
+            foreach (int score in testScores)
             {
-                if (names[j] == "Jesse")
+                if (score > 85)
                 {
-                    Console.WriteLine(names[j]);
-                    Console.Read();
+                    passingScores.Add(score);
                 }
             }
+            Console.WriteLine(passingScores.Count);
+            Console.ReadLine();
+
         }
     }
 }
+
+    
+
