db74019e336f388f6353bcd6e7927698fe589ff9
Nauris Mics <109521261+Mics90@users.noreply.github.com>
2023-02-20 13:19:50 +0000

    Iteration example

---
+++ Iteration/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int[] testscores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+            //for (int i = 0; i < testscores.Length; i++)
+            //{
+            //    if (testscores[i] > 85)
+            //    {
+            //        Console.WriteLine("Passing test score: " + testscores[i]);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
+
+            for (int j = 0; j < names.Length; j++)
+            {
+                if (names[j] == "Jesse")
+                {
+                    Console.WriteLine(names[j]);
+                    Console.Read();
+                }
+            }
+        }
+    }
+}
