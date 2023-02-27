e6ae8621942a8df46b0028de39b38acb0485daea
Nauris Mics <109521261+Mics90@users.noreply.github.com>
2023-02-27 11:55:22 +0000

    Methods

---
+++ MethodAssignment/Math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Math
+    {
+        public static int addition(int x, int y)
+        {
+            return x + y;
+        }
+        public static int substraction(int x, int y)
+        {
+            return x - y;
+        }
+        public static int multiplication(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ MethodAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select a number to add");
+            int a = Math.addition(2, Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Addition result is : " + a);
+            Console.WriteLine("Please select a number to substract");
+            int b = Math.substraction(8, Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Substraction result is : " + b);
+            Console.WriteLine("Please select a number to multiply");
+            int c = Math.multiplication(3, Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Multiplication result is : " + c);
+
+            Console.ReadLine();
+        }
+    }
+}
