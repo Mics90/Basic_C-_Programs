20b21e22405279b5d94c9042a02d049ed47a1aed
Nauris Mics <109521261+Mics90@users.noreply.github.com>
2023-03-06 10:39:35 +0000

    classes and math 2

---
+++ Classes_and_Methods_Assignment_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            
+            {
+                
+                Console.WriteLine("Please enter first number!");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter second number (optional)");
+
+                string b = Console.ReadLine();
+                if (b.Equals(string.Empty))
+                {
+                    Console.WriteLine("Math result: " + a);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    int c = math.addition(a, Convert.ToInt32(b));
+                    Console.WriteLine("Result is : " + c);
+                    Console.Read();
+                }
+
+                
+
+            }
+           
+        }
+    }
+}
---
+++ Classes_and_Methods_Assignment_2/math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_Assignment_2
+{
+    class math
+    {
+        public static int addition(int a, int b)
+        {
+         
+            return a + b;
+        }
+    }
+}
