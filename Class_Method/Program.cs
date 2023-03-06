3fdecad6c23ed641eb8b87392bef9470096be115
Nauris Mics <109521261+Mics90@users.noreply.github.com>
2023-03-06 12:13:10 +0000

    class method

---
+++ Class_Method/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            math.Addition();
+            Console.WriteLine("Type number for \"a\" value");
+            math.Addition(Convert.ToInt32(Console.ReadLine()), 14);
+
+
+            int z = math.Division(569, 13);
+            double m = math.Division(19.62, 3.2);
+            Console.WriteLine("Integer division : " + z);
+            Console.WriteLine("Double division : " + m);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class_Method/math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method
+{
+    class math
+    {
+        public static void Addition(int a = 3, int b = 5)
+        {
+            int c = (a + b) / 2;
+            Console.WriteLine("a+b/2 b=14 \nResult:" + c);
+        }
+        public static int Division(int x, int y)
+        {
+            return x / y;
+
+        }
+        public static double Division(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
