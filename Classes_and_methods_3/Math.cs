5104d00c4efd1b339efa01dcaac563948023f04e
Nauris Mics <109521261+Mics90@users.noreply.github.com>
2023-03-06 11:41:58 +0000

    Classes and methods

---
+++ Classes_and_methods_3/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_methods_3
+{
+    class Math
+    {
+        public static void method(int a = 12, int b = 16)
+        {
+
+            int c = a / 4;
+            Console.WriteLine("Result of a / 4,where \"a\" is " + a + " : " + c + "\nsecond integer is: " + b);
+
+        }
+    }
+}
---
+++ Classes_and_methods_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_methods_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math.method();
+            Math.method(156, 2);
+            Math.method(a: 360, b: 10);
+
+            Console.ReadLine();
+        }
+    }
+}
