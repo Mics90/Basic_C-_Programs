200a25f11841973a4b791fb1414071f4a60f692f
Nauris Mics <109521261+Mics90@users.noreply.github.com>
2023-03-06 09:41:16 +0000

    Classes and methods

---
+++ Classes_and_methods_assignment_1/Math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_methods_assignment_1
+{
+    class Math
+    {
+        public static int Integer(int a, int b)
+        {
+            return a + b;
+        }
+        public static decimal Decimal(decimal a, decimal b)
+        {
+            return Convert.ToDecimal(a / b);
+        }
+        public static int String(string a, string b)
+        {
+            return Convert.ToInt32(a) - Convert.ToInt32(b);
+        }
+    }
+}
---
+++ Classes_and_methods_assignment_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_methods_assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Math.Integer(10, 6);
+            Console.WriteLine("10 + 6 is : " + a);
+
+            decimal b = Math.Decimal(90, 22);
+            Console.WriteLine("90/20  division result is : " + b);
+
+
+            int c = Math.String("9999", "666");
+            Console.WriteLine("9999 - 666 result is : " + c);
+            Console.Read();
+        }
+    }
+}
