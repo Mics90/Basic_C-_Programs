4e0b7b64c7c32bfe1905e2648ac2d5708cf88bdf
Nauris Mics <109521261+Mics90@users.noreply.github.com>
2023-03-12 16:16:11 +0000

    ...

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -10,21 +10,20 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Game game = new Game();
-            game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
-            game.ListPlayers();
-           
-            Console.ReadLine();
+
             Deck deck = new Deck();
             
-           // deck.Shuffle(3);
-           //foreach (Card card in deck.Cards)
-           // {
-           //     Console.WriteLine(card.Face + " of " + card.Suit);
-           // }
+            deck.Shuffle(3);
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
 
-           // Console.WriteLine(deck.Cards.Count);
-           // Console.ReadLine();
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
+
+           
+
         }
 
     }
