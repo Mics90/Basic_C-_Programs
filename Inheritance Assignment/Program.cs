eb88e9e1e376838575906ec1bfe9ce71a0399283
Nauris Mics <109521261+Mics90@users.noreply.github.com>
2023-03-12 15:50:30 +0000

    ...

---
+++ Inheritance Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            var Employee = new Employee(); 
+
+            Employee.FirstName = "Student";
+            Employee.LastName = "Sample";
+            Employee.SayName();
+
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
