1803a94bf0e4abacc207da77a2ea6ca195e03afc
Nauris Mics <109521261+Mics90@users.noreply.github.com>
2023-03-21 13:13:28 +0000

    Exception Handling

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Exception_Handling
+{
+   class Program
+    {
+        static void Main()
+
+        {
+
+            
+            try
+            {
+                bool ValidAge = false;
+                int YourAge = 0;
+                while (!ValidAge)
+                {
+                    Console.WriteLine("Please type in your age");
+                    ValidAge = int.TryParse(Console.ReadLine(), out YourAge);
+                    if (!ValidAge) throw new Exception();
+                    if (YourAge<0)
+                    {
+                        throw new NegativeException();
+                        Console.ReadLine();
+                        return;
+
+                    }
+                    if (YourAge == 0)
+                    {
+                        throw new ZeroException();
+                        Console.ReadLine();
+                        return;
+
+                    }
+
+
+                }
+                Double YearOfBirth = Convert.ToDouble(DateTime.Now.Year) - YourAge;
+                Console.WriteLine("You was born in: " + YearOfBirth);
+                Console.ReadLine();
+            }
+
+            
+            
+            
+            catch (NegativeException)
+            {
+                
+                Console.WriteLine("Please enter positive numbers");
+                Console.ReadLine();
+                return;
+            }
+            catch (ZeroException)
+            {
+
+                Console.WriteLine("0 is not a valid age!");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ops something went wrong!");
+                Console.ReadLine();
+                return;
+            }
+            
+           
+        }
+    }
+}
--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -33,8 +33,20 @@ namespace TwentyOne
            
             Console.WriteLine("Welcome to the {0}. Lets start by telling me your name.",casinoName);
             string playerName = Console.ReadLine();
-            Console.WriteLine("And how much money did you bring today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals");
+
+            }
+
+            
+
+
             Console.WriteLine("Hello, {0}. Would yoy like to join a game of 21 right now?", playerName);
             string answer = Console.ReadLine().ToLower();
             if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
@@ -49,11 +61,28 @@ namespace TwentyOne
 
                 Game game = new TwentyOneGame();
 
+
                 game += player;
                 player.isActivelyPlaying = true;
                 while (player.isActivelyPlaying || player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch(FraudException)
+                    {
+                        Console.WriteLine("Security! Kick this person out.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch(Exception)
+                    {
+                        Console.WriteLine("An error occurred.Please contact your system administrator.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing!");
