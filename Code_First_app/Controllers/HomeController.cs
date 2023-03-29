828fbff99966adf26d9f669b0cbf2844379c41a3
Nauris Mics <109521261+Mics90@users.noreply.github.com>
2023-03-29 13:51:48 +0100

    Code_First

---
+++ Code_First_app/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Code_First_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Code_First_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+            return View();
+        }
+
+       
+    }
+}
---
+++ Code_First_app/Models/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Code_First_app.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
